b8c887c397882bdc485a7f6c00d9085156653d2a
aditi <aditiverma1009@gmail.com>
2020-09-04 18:28:16 +0530

    add validation

--- src/products/dto/products.dto.ts
+++ src/products/dto/products.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateProductDto {
+    @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
     category: string;
 }
   
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,6 +8,8 @@ import {
   Put,
   Delete,
   Patch,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ListAllEntities, Product } from './products.model';
 import { ProductsService } from './products.service';
@@ -17,6 +19,7 @@ import { CreateProductDto, UpdateProductDTO } from './dto/products.dto';
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
   @Post()
+  @UsePipes(ValidationPipe)
   create(@Body() createProductDto: CreateProductDto): Product {
     return this.productsService.create(createProductDto);
   }
