65bd1f9d9094439e14a7f2dbdf477c56657bef50
aditi <aditiverma1009@gmail.com>
2020-09-06 20:43:26 +0530

    working state, added typeorm config, added it to app module, entity, repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ TypeOrmModule.forRoot(typeormConfig),ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsRepository } from './products.repository';
 
 @Module({  
-    imports: [],
+    imports: [TypeOrmModule.forFeature([ProductsRepository])],
     controllers: [ProductsController],
     providers: [ProductsService],
     exports: []
